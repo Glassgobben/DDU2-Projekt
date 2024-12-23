3a8060213212330785a23cfd559d34b51da3bb69
Glassgobben <markuscehic21@gmail.com>
2024-12-23 19:41:31 +0100

    Klar med functioner
    Gjort funktioner som tillsammans hittar de värden som repeteras flest gånger på sidan.

--- repetitions/index.js
+++ repetitions/index.js
@@ -1,25 +1,43 @@
-let repeatedNumbers = [];
-for (let i = 0; i < cells.length; i++) {
-    let status = true;
-    let object = { number: 0, repetitons: 0 };
-    for (let j = 0; j < cells.length; j++) {
-        if (cells[i].textContent == cells[j].textContent) {
-            object.number = +cells[i].textContent;
-            object.repetitons++;
+function numberList() {
+    let repeatedNumbers = [];
+    for (let i = 0; i < cells.length; i++) {
+        let status = true;
+        let object = { number: 0, repetitions: 0 };
+        for (let j = 0; j < cells.length; j++) {
+            if (cells[i].textContent == cells[j].textContent) {
+                object.number = +cells[i].textContent;
+                object.repetitions++;
+            }
+        }
+        if (status) {
+            repeatedNumbers.push(object)
+            status = false;
         }
     }
-    if (status) {
-        repeatedNumbers.push(object)
-        status = false;
-    }
-}
-
-for (let i = 0; i < repeatedNumbers.length; i++) {
-    for (let j = 0; j < repeatedNumbers.length; j++) {
-        if (i != j && repeatedNumbers[i].number == repeatedNumbers[j].number) {
-            repeatedNumbers.splice(j, 1);
+    for (let i = 0; i < repeatedNumbers.length; i++) {
+        for (let j = 0; j < repeatedNumbers.length; j++) {
+            if (i != j && repeatedNumbers[i].number == repeatedNumbers[j].number) {
+                repeatedNumbers.splice(j, 1);
+            }
         }
     }
+    return repeatedNumbers;
 }
 
-console.log(repeatedNumbers)
+function mostRepeated() {
+    let list = numberList();
+    let winner = list[0];
+    for (let i = 0; i < list.length; i++) {
+        if (winner.repetitions < list[i].repetitions) {
+            winner = [list[i]];
+        }
+    }
+    for (let i = 0; i < list.length; i++) {
+        if (winner[0].repetitions === list[i].repetitions && winner[0].number != list[i].number) {
+            winner.push(list[i]);
+        }
+    }
+    return winner;
+}
+
+console.log(mostRepeated());
